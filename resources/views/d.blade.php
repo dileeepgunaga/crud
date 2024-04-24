27d233325a343756f792e2ba3412fafb8c06fa76
preeti <preeti@email.com>
2024-04-24 19:09:06 +0530

    changes

--- resources/views/d.blade.php
+++ resources/views/d.blade.php
@@ -10,7 +10,7 @@
     <table border="1">
         <thead>
             <tr>
-                <th>ID</th>
+                
                 <th>Name</th>
                 <th>Email</th>
                 <th>Mobile</th>
@@ -20,7 +20,7 @@
         <tbody>
             @foreach($users as $user)
             <tr>
-                <td>{{ $user->id }}</td>
+            
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
                 <td>{{ $user->mobile }}</td>
