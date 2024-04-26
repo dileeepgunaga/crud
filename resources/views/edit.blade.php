0953a295777ca4671c44a00ba7b3947daf7bf379
preeti <preeti@email.com>
2024-04-26 21:10:24 +0530

    changes

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Update Profileeeeeee</title>
+    <title>Update Profileeeeee</title>
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <link href="{{ asset('css/s.css') }}" rel="stylesheet">
 </head>
