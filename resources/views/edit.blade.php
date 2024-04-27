fd703de34010fe4d749ad57de54c43e802627f9d
preeti <preeti@email.com>
2024-04-27 08:40:16 +0530

    changes

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Update Profilere</title>
+    <title>Update Profilee</title>
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <link href="{{ asset('css/s.css') }}" rel="stylesheet">
 </head>
